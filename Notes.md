ac033362789949ef4b6285451310cb9f47715c43
kairavkkp <kairavpithadia13@gmail.com>
2021-06-24 17:07:52 +0530

    Basic graphql files generated by nest cli

---
+++ Notes.md
@@ -0,0 +1,9 @@
+## Nest CLI
+```cmd
+npm i -g @nestjs/cli
+```
+
+## GraphQL needed packages
+```cmd
+$ npm i @nestjs/graphql graphql-tools graphql apollo-server-express
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { APIModule } from './api/api.module';
+import { GraphModule } from './graph/graph.module';
 
 @Module({
-  imports: [APIModule],
+  imports: [APIModule, GraphModule],
 })
 export class AppModule {}
